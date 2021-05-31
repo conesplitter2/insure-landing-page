ced33efe1965d489902b86816f4214101d59376f
conesplitter2 <59017526+conesplitter2@users.noreply.github.com>
2021-05-31 04:28:36 +0100

    Add files via upload

---
+++ src/App.vue
@@ -0,0 +1,802 @@
+<template>
+<nav class="navbar navbar-expand-lg navbar-light navbar-1 bg-light">
+  <div class="container-fluid">
+
+    <a class="navbar-brand" href="#">
+      <img src="./assets/images/logo.svg" alt="Logo" style="padding-left: 70px">
+    </a>
+    <button class="navbar-toggler navbar-toggler-right collapsed" type="button" data-bs-toggle="collapse" data-bs-target="#navbarNav" aria-controls="navbarNav" aria-expanded="false" aria-label="Toggle navigation">
+        <span class="my-1 mx-2 close">
+          <img src="./assets/images/icon-close.svg"  alt="hamburger close">
+        </span>
+        <span class="navbar-toggler-icon">
+          <img src="./assets/images/icon-hamburger.svg"  alt="hamburger open">
+        </span>
+
+
+
+    </button>
+    <div class="collapse navbar-collapse" id="navbarNav">
+      
+      <ul class="navbar-nav ml-auto w-100 justify-content-end">
+        <li class="nav-item">
+          <a class="nav-link" aria-current="page" href="#"><p class="navOptions">HOW WE WORK</p></a>
+        </li>
+        <li class="nav-item">
+          <a class="nav-link" href="#"><p class="navOptions">BLOG</p></a>
+        </li>
+        <li class="nav-item">
+          <a class="nav-link" href="#"><p class="navOptions">ACCOUNT</p></a>
+        </li>
+        <li class="nav-item">
+          <a class="nav-link" href="#"><p class="navBoxLink">VIEW PLANS</p></a>
+        </li>
+      </ul>
+      <img src="./assets/images/bg-pattern-mobile-nav.svg" width = "350" alt="NavBar pattern overlay" class="NavOverLay">
+
+    </div>
+  </div>
+</nav>
+
+<div class="topSection">
+  <div class="topLeft">
+    <div class="topLeftTextArea">
+        <hr size="4" width="25%" class="topLeftLine">
+        <img src="./assets/images/bg-pattern-intro-left-mobile.svg" alt="left mobile intro pattern" class="topLeftMobileOverlay">
+        <h1 class="topLeftTitle">Humanizing <br>your insurance.</h1>
+        <p class="topLeftText">Get your life insurance coverage easier and faster. We blend our expertise <br>
+          and technology to help you find the plan that’s right for you. Ensure you <br>
+          and your loved ones are protected.</p>
+          <a href="" style="text-decoration: none;">
+            <p class="topLeftTextBox">VIEW PLANS</p>
+          </a>
+          
+    </div>
+    <img src="./assets/images/bg-pattern-intro-left-desktop.svg" alt="left desktop intro pattern" class="topLeftImage">
+    <img src="./assets/images/bg-pattern-intro-right-mobile.svg" alt="right mobile intro pattern" class="topRightMobileOverlay">
+  </div>
+  
+  <div class="topRight">
+    <img src="./assets/images/image-intro-desktop.jpg" alt="intro desktop image" class="topRightImage">
+    <img src="./assets/images/image-intro-mobile.jpg" alt="intro mobile image" class="topRightImageMobile">
+    <img src="./assets/images/bg-pattern-intro-right-desktop.svg" alt="right desktop pattern" class="topRightOverlay">
+  </div>
+</div>
+
+<div class="middleSection">
+
+    <hr size="4" width="10%" class="middleLine">
+    <h2 class="middleTitle">We're different</h2>
+
+  <div class="middleBlocks">
+    <div class="middleBlock1">
+        <img src="./assets/images/icon-snappy-process.svg" alt="icon snappy process" class="middleBlockIcon">
+        <h3 class="middleBlockHeader">Snappy Process</h3>
+        <p class="middleBlockText">Our application process can be completed in minutes, not hours. Don’t get stuck filling in tedious forms.</p>
+
+    </div>
+    <div class="middleBlock2">
+        <img src="./assets/images/icon-affordable-prices.svg" alt="icon affordable prices" class="middleBlockIcon">
+        <h3 class="middleBlockHeader" >Affordable Prices</h3>
+        <p class="middleBlockText">We don’t want you worrying about high monthly costs. Our prices may be low, but we still offer the best coverage possible.</p>
+
+    </div>
+    <div class="middleBlock3">
+        <img src="./assets/images/icon-people-first.svg" alt="icon people first" class="middleBlockIcon">
+        <h3 class="middleBlockHeader">People First</h3>
+        <p class="middleBlockText">Our plans aren’t full of conditions and clauses to prevent payouts. We make sure you’re covered when you need it.</p>
+    </div>
+  </div>
+</div>
+
+<div class="middleBox">
+  <div class="middleBoxLeft">
+    <h2 class="middleBoxLeftText">Find out more <br>about how we work</h2>
+    <img src="./assets/images/bg-pattern-how-we-work-mobile.svg" alt="how we work mobile pattern" class="middleBoxOverlayMobile">
+  </div>
+  <div class="middleBoxRight">
+    <img src="./assets/images/bg-pattern-how-we-work-desktop.svg" alt="how we work desktop pattern" class="middleBoxOverlay">
+    <div>
+      
+    </div>
+    <a href="" style="text-decoration: none;">
+        <p class="middleBoxRightText">HOW WE WORK</p>
+    </a>
+
+  </div>
+
+</div>
+<div class="bottomLogoSocial">
+  <div class="bottomLogo">
+      <img src="./assets/images/logo.svg" alt="Logo">
+
+  </div>
+  <div class="bottomSocial">
+    <a href="" class="socialMediaIcon">
+      <img src="./assets/images/icon-facebook.svg" alt="Facebook icon">
+    </a>
+    <a href="" class="socialMediaIcon">
+      <img src="./assets/images/icon-twitter.svg" alt="Twitter icon">
+    </a>
+    <a href="" class="socialMediaIcon">
+      <img src="./assets/images/icon-pinterest.svg" alt="Pinterest icon">
+    </a>
+    <a href="" class="socialMediaIcon">
+      <img src="./assets/images/icon-instagram.svg" alt="Instagram icon">
+    </a>
+
+
+  </div>
+</div>
+    <hr size="3" width="100%" class="FooterLine">
+
+<div class="Footer">
+  <div class="FooterColumn1">
+    <h4 class="FooterTitle">our company</h4>
+    <a href="" class="FooterLink">
+      <p>how we work</p>
+    </a>
+    <a href="" class="FooterLink">
+      <p>why insure?</p>
+    </a>
+    <a href="" class="FooterLink">
+      <p>view plans</p>
+    </a>
+    <a href="" class="FooterLink">
+      <p>reviews</p>
+    </a>
+
+  </div>
+  <div class="FooterColumn2">
+    <h4 class="FooterTitle">help me</h4>
+    <a href="" class="FooterLink">
+      <p>faq</p>
+    </a>
+    <a href="" class="FooterLink">
+      <p>terms of use</p>
+    </a>
+    <a href="" class="FooterLink">
+      <p>privacy policy</p>
+    </a>
+    <a href="" class="FooterLink">
+      <p>cookies</p>
+    </a>
+    
+  </div>
+  <div class="FooterColumn3">
+    <h4 class="FooterTitle">contact</h4>
+    <a href="" class="FooterLink">
+      <p>sales</p>
+    </a>
+    <a href="" class="FooterLink">
+      <p>support</p>
+    </a>
+    <a href="" class="FooterLink">
+      <p>live chat</p>
+    </a>
+  </div>
+  <div class="FooterColumn4">
+    <h4 class="FooterTitle">others</h4>
+    <a href="" class="FooterLink">
+      <p>careers</p>
+    </a>
+    <a href="" class="FooterLink">
+      <p>press</p>
+    </a>
+    <a href="" class="FooterLink">
+      <p>licenses</p>
+    </a> 
+  </div>
+
+</div>
+</template>
+
+<script>
+
+export default {
+  name: 'App',
+  data(){
+    return{
+    }
+  }
+
+}
+</script>
+
+<style>
+@import url('https://fonts.google.com/specimen/Karla');
+@import url('https://fonts.google.com/specimen/DM+Serif+Display');
+
+/* start of general styles section */
+#app {
+  font-family: 'Karla', sans-serif;
+  -webkit-font-smoothing: antialiased;
+  -moz-osx-font-smoothing: grayscale;
+  color: black;
+  margin: 0%;
+  padding: 0%;
+}
+h1{
+    font-size: 75px;
+}
+h1, h2, h3, h4{
+    font-family: 'DM Serif Display', serif;
+
+}
+/* end of general styles section */
+
+
+/* start of NavBar Desktop*/
+.navbar-toggler>.close {
+    display:inline;
+}
+.navbar-toggler.collapsed>.close, .navbar-toggler:not(.collapsed)>.navbar-toggler-icon {
+    display:none;
+}
+
+.nav-link{
+  font-weight: 700;
+  padding-top: 5px;
+  padding-bottom: 5px;
+  font-size: 15px;
+  margin-bottom: -25px;
+
+}
+.navOptions{
+  margin-top: 3.5px;
+}
+
+.navBoxLink{
+  border: 1px solid rgb(0, 0, 0);
+  text-align: center;
+  align-items: center;
+  width: 125px;
+  padding-top: 3px;
+  padding-bottom: 4px;
+}
+.navBoxLink:hover{
+  background-color: black;
+  color: white;
+
+}
+.NavOverLay{
+  display: none;
+}
+
+.navbar{
+    z-index: 950;
+
+}
+.navbar-toggler:focus,
+.navbar-toggler:active,
+.navbar-toggler-icon:focus {
+    outline: none;
+    box-shadow: none;
+}
+
+/* end of NavBar Desktop*/
+
+
+/* start of top section Desktop*/
+.topSection{
+  background-image: linear-gradient(to bottom, hsl(256, 26%, 20%) 75%, rgb(255, 255, 255) 25%);
+  display: flex;
+  flex-wrap: wrap;
+  color: white;
+}
+
+.topLeft{
+  flex: 1.1;
+  display: flex;
+  flex-direction: column;
+  justify-content: right;
+  align-items: flex-start;
+  text-align: left;
+  position: relative;
+
+}
+.topLeftLine{
+  margin-top: -15%;
+  margin-bottom: 10%;
+  flex: 0.5;
+  background-color: white;
+}
+.topLeftTextArea{
+  margin: 30% auto;
+  position: relative;
+  flex: 1;
+}
+.topLeftTitle{
+  font-weight: 700;
+} 
+.topLeftText{
+  font-weight: 400;
+}
+.topLeftImage{
+  position: absolute;
+  top: 56.5%;
+
+}
+.topLeftTextBox{
+  border: 1px solid rgb(255, 255, 255);
+  text-align: center;
+  align-items: center;
+  color: white;
+  margin-top: 5%;
+  max-width: 145px;
+
+  padding: 6px;
+  font-weight: 700;
+}
+
+.topLeftTextBox:hover{
+  color: black;
+  background-color: white;
+}
+
+  
+.topLeftMobileOverlay{
+  display: none;
+}
+
+.topRight{
+
+  min-width: 50vw;
+  min-height: 100vh;
+  flex: 0.9;
+  position: relative;
+
+}
+.topRightImage{
+  margin-top: 19%;
+  margin-left: 4%;
+  position: relative;
+  visibility: visible;
+}
+.topRightImageMobile{
+  display: none;
+}
+.topRightOverlay{
+  position: absolute;
+  top: -7%;
+  z-index: 900;
+  right: 2px
+}
+.topRightMobileOverlay{
+  display: none;
+}
+/* end of top section Desktop */
+
+
+/* start of middle section Desktop */
+
+.middleSection{
+  display: flex;
+  flex-direction: column;
+  max-width: 1190px;
+    
+  margin: 0 auto;
+
+}
+
+.middleTitle{
+  justify-content: flex-end;
+  font-weight: 700;
+  font-size: 50px;
+  margin-bottom: 5%;
+}
+
+.middleLine{
+  background-color: rgb(98, 98, 98);
+}
+.middleBlocks{
+  display: flex;
+  justify-content: space-between;
+  flex-wrap: wrap;
+  margin: auto;
+}
+.middleBlockIcon{
+  margin-bottom: 15px;
+}
+.middleBlockHeader{
+  font-weight: 700;
+}
+.middleBlockText{
+  color: gray;
+  font-weight: 400;
+}
+.middleBlock1{
+  flex: 1;
+  flex-grow: 1;
+  margin-right: 10%;
+
+}
+.middleBlock2{
+  flex: 1;
+  flex-grow: 1;
+  margin-right: 5%;
+
+
+}
+.middleBlock3{
+  flex: 1;
+  flex-grow: 1;
+
+}
+
+.middleBox{
+  display: flex;
+  justify-content: space-between;
+  margin: 2rem auto;
+  background-color: hsl(256, 26%, 20%);
+  max-width: 1190px;
+  min-height: 250px;
+
+}
+.middleBoxLeft{
+  flex: 1.5;
+  margin: auto 5%;
+  color: white;
+
+}
+.middleBoxLeftText{
+  font-weight: 700;
+  font-size: 50px;
+
+}
+
+.middleBoxRight{
+  flex: 0.5;
+  margin: auto auto;
+  text-decoration: none;
+    position: relative;
+
+}
+.middleBoxRightText{
+  border: 1px solid rgb(255, 255, 255);
+  text-align: center;
+  color: white;
+  max-width: 170px;
+  padding-top: 5px;
+  padding-bottom: 5px;
+  font-weight: 700;
+  position: relative;
+}
+.middleBoxRightText:hover{
+  color: black;
+  background-color: white;
+}
+
+.middleBoxOverlay{
+  position: absolute;
+  top: -187%;
+  right: 2px
+}
+
+.middleBoxOverlayMobile{
+  display: none;
+}
+.bottomLogoSocial{
+  display: flex;
+  justify-content: space-between;
+  margin: auto;
+  max-width: 1190px;
+
+}
+/* end of middle section Desktop */
+
+/* start of bottom section Desktop */
+
+.bottomLogo{
+  flex: 8;
+}
+
+.bottomSocial{
+    flex: 1;
+    display: flex;
+    justify-content: space-between;
+
+
+}
+.socialMediaIcon{
+    opacity: 0.75;
+
+}
+.socialMediaIcon:hover{
+    opacity: 1;
+
+}
+/* end of bottom section Desktop */
+
+/* start of footer section Desktop */
+
+.FooterLine{
+  max-width: 1190px;
+  margin: auto;
+  margin-top: 20px;
+  background-color: gray;
+
+}
+.Footer{
+  display: flex;
+  justify-content: space-between;
+  max-width: 1190px;
+  margin: auto;
+  margin-top: 2%;
+
+}
+
+.FooterColumn1{
+  display: flex;
+  flex-direction: column;
+  text-transform: uppercase;
+
+}
+.FooterColumn2{
+  display: flex;
+  flex-direction: column;
+  text-transform: uppercase;
+
+}
+.FooterColumn3{
+  display: flex;
+  flex-direction: column;
+  text-transform: uppercase;
+
+}
+.FooterColumn4{
+  display: flex;
+  flex-direction: column;
+  text-transform: uppercase;
+
+}
+
+.FooterTitle{
+  margin-bottom: 30px;
+  font-size: 15px;
+  font-weight: 700;
+  color: gray;
+}
+
+.FooterLink{
+  text-decoration: none;
+  color: black;
+  font-weight: 700;
+  font-size: 15px;
+
+}
+.FooterLink:hover{
+    text-decoration: underline;
+    color: black;
+}
+
+/* end of footer section Desktop */
+
+
+
+
+
+
+@media (max-width: 750px){
+  /* start of general styles section */
+  h1{
+    font-size: 50px;
+  }
+  /* end of general styles section */
+
+
+  /* start of navbar section mobile */
+  .nav-link{
+    margin-bottom: 5px;
+    text-align: center;
+    color: white;
+  }
+  .navBoxLink{
+    margin: auto auto;
+    color: white;
+    border: 1px solid white;
+    text-align: center;
+    align-items: center;
+    width: 300px;
+    padding-top: 9px;
+    padding-bottom: 9px;
+  }
+  .navOptions{
+    color: white;
+  }
+  .NavOverLay{
+    display: inline;
+    position: absolute;
+    top: 75%;
+    z-index: 900;
+  }
+   .navbar-collapse {
+    background: #2b282f;
+    min-height: 100vh;
+  }
+  /* end of navbar section mobile */
+
+  /* start of top section mobile */
+  .topSection{
+        flex-direction: column;
+        background-color: hsl(256, 26%, 20%);
+        
+
+  }
+  .topLeft{
+    order: 2;
+    background-color: hsl(256, 26%, 20%);
+    flex: 1;
+    margin-top: -50px;
+}
+.topLeftLine{
+    display: none;
+
+}
+.topLeftTextBox{
+  text-align: center;
+} 
+.topLeftTitle{
+  text-align: center;
+  margin-top: 7%;
+  z-index: 950;
+} 
+.topLeftText{
+  text-align: center;
+  color: rgb(230, 230, 230);
+}
+.topLeftTextBox{
+  margin: auto;
+}
+.topLeftImage{
+  display: none;
+}
+
+.topLeftMobileOverlay{
+  display: inline;
+  position: absolute;
+  top: -19%;
+  z-index: 900;
+}
+
+.topRight{
+    background-color: hsl(256, 26%, 20%);
+    order: 1;
+    margin-bottom: 0px;
+    flex: 1;
+    min-height: 0vh;
+
+}
+.topRightImage{
+  margin: 0;
+  display: none;
+}
+.topRightImageMobile{
+  visibility: visible;
+  display: inline;
+}
+.topRightOverlay{
+    display: none;
+
+}
+
+.topRightMobileOverlay{
+  display: inline;
+  position: absolute;
+  top: 70%;
+  z-index: 900;
+  right: 2px
+}
+/* end of top section mobile */
+
+/* start of middle section mobile */
+.middleSection{
+    margin-top: 35%;
+
+}
+.middleLine{
+  align-items: center;
+  margin: 0 auto;
+  background-color: black;
+  min-width: 150px;
+  margin-bottom: 10%;
+}
+.middleTitle{
+  margin: 0;
+  text-align: center;
+  margin-bottom: 10%;
+}
+.middleBlocks{
+    flex-direction: column;
+
+}
+.middleBlock1{
+  align-items: center;
+  text-align: center;
+  margin: auto auto;
+
+
+}
+.middleBlock2{
+  align-items: center;
+  text-align: center;
+  margin: auto auto;
+
+
+}
+.middleBlock3{
+  align-items: center;
+  text-align: center;
+  margin: auto auto;
+
+}
+
+.middleBox{
+  flex-direction: column;
+  margin-bottom: 45%;
+
+}
+.middleBoxLeftText{
+  text-align: center;
+}
+.middleBoxLeft{
+  text-align: left;
+  margin-top: 10%;
+
+}
+.middleBoxRightText{
+  width: 200px;
+}
+
+.middleBoxOverlay{
+  display: none;
+}
+.middleBoxOverlayMobile{
+  display: inline;
+  position: absolute;
+  top: 1985px;
+  right: 2px
+}
+
+.middleBoxRightText{
+  margin-bottom: 45%;
+}
+/* end of middle section mobile */
+
+/* start of bottom section mobile */
+
+.bottomLogoSocial{
+    flex-direction: column;
+    align-items: center;
+}
+.bottomSocial{
+  margin-top: 5%;
+  margin-bottom: 10%;
+  align-items: center;
+  margin-left: -9%;
+}
+.socialMediaIcon{
+  margin-right: 10%;
+}
+/* end of bottom section mobile */
+
+/* start of footer section mobile */
+.Footer{
+      flex-direction: column;
+        text-align: center;
+
+}
+.FooterColumn1{
+  margin-top: 5%;
+}
+.FooterLine{
+  margin: auto auto;
+}
+
+.FooterTitle{
+  margin-top: 8%;
+} 
+/* end of footer section mobile */
+
+}
+</style>
